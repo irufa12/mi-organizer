03521ca9837d2a6b4aba47259cd1e930fed2e30e
irufa12 <107341426+irufa12@users.noreply.github.com>
2022-06-29 21:59:02 +0700

    Update DB

---
+++ mi-organizer.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 29, 2022 at 04:57 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mi-organizer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(3, 'Makanan'),
+(4, 'Minuman'),
+(5, 'Elektronik '),
+(6, 'Otomotif'),
+(7, 'Fashion'),
+(8, 'Mainan anak-anak');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_login`
+--
+
+CREATE TABLE `tbl_login` (
+  `id_login` int(11) NOT NULL,
+  `anggota_id` varchar(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tempat_lahir` varchar(255) NOT NULL,
+  `tgl_lahir` varchar(255) NOT NULL,
+  `jenkel` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `telepon` varchar(25) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `tgl_bergabung` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`id_login`, `anggota_id`, `user`, `pass`, `level`, `nama`, `tempat_lahir`, `tgl_lahir`, `jenkel`, `alamat`, `telepon`, `email`, `tgl_bergabung`, `foto`) VALUES
+(4, 'AG003', 'irhash', '3b607996d6f72bf43c3001a9024e1272', 'Petugas', 'irhash', 'Bekasi', '1999-07-02', 'Laki-Laki', 'Vila Nusa Indah', '08833354642', 'muhammadirhash76@gmail.com', '2022-06-20', 'user_1655734004.JPG'),
+(6, 'AG005', 'enakdilirik', '46f94c8de14fb36680850768ff1b7f2a', 'Petugas', 'Fadhil', 'CITAYEM', '2012-12-12', 'Laki-Laki', 'Bojong Gede', '080129860012', 'exampel_220@gmail.com', '2022-06-29', 'user_1656471038.JPG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_lokasi`
+--
+
+CREATE TABLE `tbl_lokasi` (
+  `id_lokasi` int(11) NOT NULL,
+  `nama_lokasi` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_lokasi`
+--
+
+INSERT INTO `tbl_lokasi` (`id_lokasi`, `nama_lokasi`) VALUES
+(3, 'Hall A'),
+(4, 'Hall B'),
+(5, 'Hall C');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tenant`
+--
+
+CREATE TABLE `tbl_tenant` (
+  `id_tenant` int(11) NOT NULL,
+  `tenant_id` varchar(255) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `id_lokasi` int(11) NOT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `no_telephone` varchar(255) DEFAULT NULL,
+  `nama_tenant` varchar(255) DEFAULT NULL,
+  `perusahaan` varchar(255) DEFAULT NULL,
+  `penanggung_jawab` varchar(255) DEFAULT NULL,
+  `jml` int(11) DEFAULT NULL,
+  `tgl_masuk` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_tenant`
+--
+
+INSERT INTO `tbl_tenant` (`id_tenant`, `tenant_id`, `id_kategori`, `id_lokasi`, `logo`, `no_telephone`, `nama_tenant`, `perusahaan`, `penanggung_jawab`, `jml`, `tgl_masuk`) VALUES
+(10, 'T0010', 6, 5, 'cf69a069013e97b40c288b2173a5bd4b.jpg', '0897676554672', 'Honda Motor', 'PT Astra Honda Motor', 'Tatsumo', 4, '2022-06-25 00:09:42'),
+(13, 'T0011', 4, 3, '3e547bd190e39c59ff4d0669170410ce.jpg', '087656838441', 'Jamu', 'PT Sidomuncul', 'Soebardjo Koesomoe', 3, '2022-06-29 10:00:52'),
+(14, 'T0014', 7, 4, '0816de36f94aa59fd22130db8462d2af.jpg', '089767655467', 'ZARA', 'ZARA', 'Elvan', 3, '2022-06-29 10:01:48');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `tbl_login`
+--
+ALTER TABLE `tbl_login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indexes for table `tbl_lokasi`
+--
+ALTER TABLE `tbl_lokasi`
+  ADD PRIMARY KEY (`id_lokasi`);
+
+--
+-- Indexes for table `tbl_tenant`
+--
+ALTER TABLE `tbl_tenant`
+  ADD PRIMARY KEY (`id_tenant`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tbl_login`
+--
+ALTER TABLE `tbl_login`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_lokasi`
+--
+ALTER TABLE `tbl_lokasi`
+  MODIFY `id_lokasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_tenant`
+--
+ALTER TABLE `tbl_tenant`
+  MODIFY `id_tenant` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
